63037c17b5cfc0e2319736949117d884e4011b66
Noble Mungu <mungunoble@gmail.com>
2020-05-03 19:42:00 +0300

    added months array

--- script.js
+++ script.js
@@ -12,4 +12,18 @@ function MakeArray(n) {
  days[4] = "Wednesday"
  days[5] = "Thursday"
  days[6] = "Friday"
-  
+
+
+ months = new MakeArray(12);
+ months[1] = "January"
+ months[2] = "February"
+ months[3] = "March"
+ months[4] = "April"
+ months[5] = "May"
+ months[6] = "June"
+ months[7] = "July"
+ months[8] = "August"
+ months[9] = "September"
+ months[10] = "October"
+ months[11] = "November"
+ months[12] = "December" 
